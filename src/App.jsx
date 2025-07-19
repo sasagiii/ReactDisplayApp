ff3c83a2abbc0cbb1cefa8dd04c37de0a643cd60
User <samibenmaammargb1@gmail.com>
2025-07-19 18:29:28 +0200

    image display home page correction

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import React from "react";
 function App() {
   return (
     <AppProvider>
-      <Router basename="/ReactDisplayApp">
+      <Router basename="/ReactDisplayApp/">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/Experience" element={<ExperiencePage />} />
