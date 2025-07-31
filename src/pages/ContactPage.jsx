04c65df435bdb62160821b8e4adb320d9f5a9099
User <samibenmaammargb1@gmail.com>
2025-07-31 20:28:44 +0200

    Contact Page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,20 @@
 import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
+import Styles from "./ContactPage.module.css";
 
 export default function ContactPage() {
   return (
-    <div>
+    <div className={Styles.PageDiv}>
       <NavBar />
-      contact Page
+      <div className={Styles.Container}>
+        <h1>Just use LinkedIn 😅</h1>
+        <a
+          href="https://www.linkedin.com/in/sami-ben-profile/?locale=en_US"
+          target="_blank"
+        >
+          <img src="./LinkedIn_icon.webp" width="50" height="50"></img>
+        </a>
+      </div>
       <Footer />
     </div>
   );
